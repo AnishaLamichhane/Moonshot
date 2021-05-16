611108b56879e5ed4a042594050852333ac9e098
Anisha Lamichhane <ansalamichhane111@gmail.com>
2021-05-16 14:50:41 +0545

    Initial Commit

---
+++ project 8/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  project 8
+//
+//  Created by Anisha Lamichhane on 5/16/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ project 8/project_8App.swift
@@ -0,0 +1,17 @@
+//
+//  project_8App.swift
+//  project 8
+//
+//  Created by Anisha Lamichhane on 5/16/21.
+//
+
+import SwiftUI
+
+@main
+struct project_8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
