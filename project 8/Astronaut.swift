d9949a59969d95db892cc811ac54baa26b93e80d
Anisha Lamichhane <ansalamichhane111@gmail.com>
2021-05-18 12:50:38 +0545

    Loading a specific kind of Codable data

---
+++ project 8/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  project 8
+//
+//  Created by Anisha Lamichhane on 5/18/21.
+//
+
+import Foundation
+ 
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
